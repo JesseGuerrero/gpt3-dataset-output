c1924ec69a9075b9313344f90bd747bf59d66805
Jesse Guerrero <jesseguerrero1991@gmail.com>
2023-03-14 00:11:04 -0500

    Updated with latest datasets

--- synthetic/GPT3-API.py
+++ synthetic/GPT3-API.py
@@ -18,30 +18,30 @@ openai.api_key = open_file('openaiapikey.txt')
 import pandas as pd
 articles = pd.read_excel('articles.xlsx')
 
-titles = articles['Title'].tolist()[:50]
+titles = articles['Title'].tolist()[62_999:]
 
 
 
-def gpt3_completion(prompt, engine='text-babbage-001', temp=1.0, top_p=1.0, tokens=128, freq_pen=0.0,
-                    pres_pen=0.0, stop=['asdfasdf', 'asdasdf']):
+def gpt3_completion(message, model='gpt-3.5-turbo', temp=1.0, top_p=1.0, tokens=128, freq_pen=0.0,
+                    pres_pen=0.0):
     max_retry = 5
     retry = 0
-    prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
+    message = message.encode(encoding='ASCII', errors='ignore').decode()
     while True:
         try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=[{"role": "user", "content": message}],
                 temperature=temp,
                 max_tokens=tokens,
                 top_p=top_p,
                 frequency_penalty=freq_pen,
                 presence_penalty=pres_pen,
-                stop=stop)
-            text = response['choices'][0]['text'].strip()
+            )
+            text = response['choices'][0]['message']['content'].strip()
             # text = re.sub('\s+', ' ', text)
             filename = '%s_gpt3.txt' % time()
-            save_file('gpt3_logs/%s' % filename, prompt + '\n\n==========\n\n' + text)
+            save_file('gpt3_logs/%s' % filename, message + '\n\n==========\n\n' + text)
             return text
         except Exception as oops:
             retry += 1
@@ -54,7 +54,7 @@ import json
 if __name__ == '__main__':
     count = 0
     jsonResponses = {}
-    with open("responsesSample.json", "r") as jsonFile:
+    with open("responsesSample35.json", "r") as jsonFile:
         jsonResponses = dict(json.load(jsonFile))
     for title in titles:
         count += 1
@@ -64,9 +64,9 @@ if __name__ == '__main__':
         jsonResponses[title] = [completion]
         print('\n' + str(count) + " " + prompt)
         print(completion)
-        if count % 5_000 == 0:
-            with open("responsesSample.json", "w") as jsonFile:
+        if count % 500 == 0:
+            with open("responsesSample35.json", "w") as jsonFile:
                 json.dump(jsonResponses, jsonFile)
-    with open("responsesSample.json", "w") as jsonFile:
+    with open("responsesSample35.json", "w") as jsonFile:
         json.dump(jsonResponses, jsonFile)
     # print(count)
